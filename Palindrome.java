02a539d474d9ddcddc2c2ccae718a0058ceecb20
manarelayadi <>
2021-04-06 20:49:26 +0000

    Done : all tests are okey

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,18 @@
-public class Palindrome{
-    public static boolean is_palindrome(String s)
-    {
-        // add your code here
-        return true;
+public class Palindrome {
+  public static boolean is_palindrome(String s) {
+    String reverse = "";
+    for (int i = s.length() - 1; i >= 0; i--) {
+      reverse = reverse + s.charAt(i);
     }
+    if (s.equalsIgnoreCase(reverse)) { 
+      return true;
+      
+    } 
+    else {
+      return false;
+      
+    }
+    
+
+  }
 }
